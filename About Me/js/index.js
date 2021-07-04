38bfed8bca1432601d3c2764c6ae4345e76be5a7
Viki <vikic3010@gmail.com>
2021-07-04 16:00:58 -0700

    overlayAdd

--- About Me/js/index.js
+++ About Me/js/index.js
@@ -1,15 +1,8 @@
 console.log("Your index.js file is loaded correctly!");
 
 $(document).ready(function(){
-// $(".intro").hover(function(){
-  // $(this).css("color", "blue");
-  // },
-  // function(){
-    // $(this).css("color", "black");
-  // });
-
   $(".projects").hover(function(){
-    $(this).css("color", "yellow");
+    $(this).css("color", "black");
     },
     function(){
       $(this).css("color", "white");
--- Home/js/index.js
+++ Home/js/index.js
@@ -1,3 +1,11 @@
 console.log("Your index.js file is loaded correctly!");
 
 $(document).ready(function(){
+    $('#viki').hover(function(){
+        if ($(this).attr("src") == "images/SafeTravel.png"){
+            $(this).attr("src", $(this).attr("src").replace("SafeTravel.png", "SafeTravel1.png"));
+        } else {
+            $(this).attr("src", $(this).attr("src").replace("SafeTravel1.png", "SafeTravel.png"));
+        }
+    })
+});
